9295914ac25881e74de8a3af16c5a843ca5390be
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2017-04-19 17:42:56 -0700

    Create Asamyuthatypes.java

---
+++ Asamyuthatypes.java
@@ -0,0 +1,373 @@
+package com.amer.dancee;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.MenuItem;
+import android.R;
+
+public class Asamyuthatypes extends Activity {
+	Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19,b20,b21,b22,b23,b24,b25,b26,b27,b28,b29;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_asamyuthatypes);
+		
+    b1=(Button) findViewById(R.id.but1);	
+    b2=(Button) findViewById(R.id.but2);	
+    b3=(Button) findViewById(R.id.but3);	
+    b4=(Button) findViewById(R.id.but4);	
+    b5=(Button) findViewById(R.id.but5);	
+    b6=(Button) findViewById(R.id.but6);	
+    b7=(Button) findViewById(R.id.but7);	
+    b8=(Button) findViewById(R.id.but8);	
+    b9=(Button) findViewById(R.id.but9);	
+    b10=(Button) findViewById(R.id.but10);	
+    b11=(Button) findViewById(R.id.but11);	
+    b12=(Button) findViewById(R.id.but12);	
+    b13=(Button) findViewById(R.id.but13);	
+    b14=(Button) findViewById(R.id.but14);	
+    b15=(Button) findViewById(R.id.but15);	
+    b16=(Button) findViewById(R.id.but16);	
+    b17=(Button) findViewById(R.id.but17);	
+    b18=(Button) findViewById(R.id.but18);	
+    b19=(Button) findViewById(R.id.but19);	
+    b20=(Button) findViewById(R.id.but20);	
+    b21=(Button) findViewById(R.id.but21);	
+    b22=(Button) findViewById(R.id.but22);	
+    b23=(Button) findViewById(R.id.but23);	
+    b24=(Button) findViewById(R.id.but24);	
+    b25=(Button) findViewById(R.id.but25);	
+    b26=(Button) findViewById(R.id.but26);	
+    b27=(Button) findViewById(R.id.but27);	
+    b28=(Button) findViewById(R.id.but28);	
+    b29=(Button) findViewById(R.id.but29);	
+    
+ b29.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il29=new Intent(Asamyuthatypes.this,Flag.class);
+			startActivity(il29);
+
+		}
+	});
+    
+    
+    
+    b1.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il1=new Intent(Asamyuthatypes.this,Fla.class);
+			startActivity(il1);
+
+		}
+	});
+    
+    b2.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il2=new Intent(Asamyuthatypes.this,Flagwiththree.class);
+			startActivity(il2);
+
+		}
+	});
+    
+    b3.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il3=new Intent(Asamyuthatypes.this,Halfflag.class);
+			startActivity(il3);
+
+		}
+	});
+    
+    b4.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il4=new Intent(Asamyuthatypes.this,Scissorface.class);
+			startActivity(il4);
+
+		}
+	});
+    
+    b5.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il5=new Intent(Asamyuthatypes.this,Peacock.class);
+			startActivity(il5);
+
+		}
+	});
+    
+    b6.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il6=new Intent(Asamyuthatypes.this,Halfmoon.class);
+			startActivity(il6);
+
+		}
+	});
+    
+    b7.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il7=new Intent(Asamyuthatypes.this,Bent.class);
+			startActivity(il7);
+
+		}
+	});
+    
+    b8.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il8=new Intent(Asamyuthatypes.this,Parrotsbeak.class);
+			startActivity(il8);
+
+		}
+	});
+    
+    b9.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il9=new Intent(Asamyuthatypes.this,Fist.class);
+			startActivity(il9);
+
+		}
+	});
+    
+    b10.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il10=new Intent(Asamyuthatypes.this,Peak.class);
+			startActivity(il10);
+
+		}
+	});
+    
+    b11.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il11=new Intent(Asamyuthatypes.this,Elephantapple.class);
+			startActivity(il11);
+
+		}
+	});
+    
+    b12.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il12=new Intent(Asamyuthatypes.this,Braceletface.class);
+			startActivity(il12);
+
+		}
+	});
+    
+    b13.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il13=new Intent(Asamyuthatypes.this,Needle.class);
+			startActivity(il13);
+
+		}
+	});
+    
+    b14.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il14=new Intent(Asamyuthatypes.this,Digitofthemoon.class);
+			startActivity(il14);
+
+		}
+	});    
+    b15.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il15=new Intent(Asamyuthatypes.this,Lotusbud.class);
+			startActivity(il15);
+
+		}
+	});    
+    b16.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il16=new Intent(Asamyuthatypes.this,Snakehood.class);
+			startActivity(il16);
+
+		}
+	});
+    
+    b17.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il17=new Intent(Asamyuthatypes.this,Deerhead.class);
+			startActivity(il17);
+
+		}
+	});
+    
+    b18.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il18=new Intent(Asamyuthatypes.this,Lionface.class);
+			startActivity(il18);
+
+		}
+	});    
+    b19.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il19=new Intent(Asamyuthatypes.this,Tail.class);
+			startActivity(il19);
+
+		}
+	});
+    
+    b20.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il=new Intent(Asamyuthatypes.this,Square.class);
+			startActivity(il);
+
+		}
+	});
+    
+    b21.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il20=new Intent(Asamyuthatypes.this,Square.class);
+			startActivity(il20);
+
+		}
+	});
+    
+    b22.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il22=new Intent(Asamyuthatypes.this,Bee.class);
+			startActivity(il22);
+
+		}
+	});
+    
+    b23.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il23=new Intent(Asamyuthatypes.this,Swanbeak.class);
+			startActivity(il23);
+
+		}
+	});
+    
+    b24.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il24=new Intent(Asamyuthatypes.this,Swanwing.class);
+			startActivity(il24);
+
+		}
+	});
+    
+    b25.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il25=new Intent(Asamyuthatypes.this,Pincers.class);
+			startActivity(il25);
+
+		}
+	});
+    
+    b26.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il26=new Intent(Asamyuthatypes.this,Bud.class);
+			startActivity(il26);
+
+		}
+	});
+    
+    b27.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il27=new Intent(Asamyuthatypes.this,Cock.class);
+			startActivity(il27);
+
+		}
+	});
+    
+    b28.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent il28=new Intent(Asamyuthatypes.this,Trident.class);
+			startActivity(il28);
+
+		}
+	});
+    
+   
+	}
+}
